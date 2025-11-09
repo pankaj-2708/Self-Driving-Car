a44448c69668f09a769ee6e3bb47e4764a418e1b
pankaj-2708 <pankajmaulekhi32@gmail.com>
2025-11-09 19:44:30 +0530

    simulation added

--- src/models_to_onnx.py
+++ src/models_to_onnx.py
@@ -1,7 +1,7 @@
 import onnxruntime as ort
 import tensorflow as tf
 import tf2onnx
-
+from custom_objects import mean_iou,dice_loss,tpr,fpr,dice_coefficient
 print(ort.get_device())
 
 
@@ -20,5 +20,20 @@ spec = (tf.TensorSpec((1, *input_shape), tf.float32, name="input"),)
 
 tf2onnx.convert.from_keras(wrapped_model, input_signature=spec, opset=17,output_path="models/sterring_angle.onnx")
 
-# with open("models/sterring_angle.onnx", "wb") as f:
-#     f.write(onnx_model.SerializeToString())
+
+# lane detection model conversion
+lane_model = tf.keras.models.load_model("./models/lane_detection.h5", custom_objects={
+                       "dice_loss": dice_loss,
+                       "mean_iou": mean_iou,
+                       "dice_coefficient": dice_coefficient,
+                       "tpr": tpr,
+                       "fpr": fpr
+                   },compile=False)
+input_shape_lane = lane_model.input_shape[1:]   # removes batch dim (None, H, W, C)
+# Create new functional wrapper
+inp_lane = tf.keras.Input(shape=input_shape_lane, name="input")
+out_lane = lane_model(inp_lane)
+wrapped_model_lane = tf.keras.Model(inputs=inp_lane, outputs=out_lane)
+# Define TensorSpec (batch=1)
+spec_lane = (tf.TensorSpec((1, *input_shape_lane), tf.float32, name="input"),)
+tf2onnx.convert.from_keras(wrapped_model_lane, input_signature=spec_lane, opset=17,output_path="models/lane_detection.onnx")
--- src/simulation.py
+++ src/simulation.py
@@ -1,30 +1,33 @@
 import cv2
 import os
 import time
+import pandas as pd
 import numpy as np
 from tensorflow.keras.models import load_model
 from ultralytics import YOLO
 import supervision as sv
+import onnxruntime as ort
 from custom_objects import mean_iou,dice_loss,tpr,fpr,dice_coefficient
+print(ort.get_device())
+
 
 def load_object_detection_model():
-    model=YOLO("models/yolo11n.pt")
+    model=YOLO("models/yolo11n.pt").to("cuda")
     return model
 
 def load_lane_detection_model():
-    model_path=os.path.join("models","lane_detection.h5")
-    model=load_model(model_path,custom_objects={
-                       "dice_loss": dice_loss,
-                       "mean_iou": mean_iou,
-                       "dice_coefficient": dice_coefficient,
-                       "tpr": tpr,
-                       "fpr": fpr
-                   })
+    model=model = ort.InferenceSession(
+    "models/lane_detection.onnx",
+    providers=["CUDAExecutionProvider"]   # GPU
+)
     return model
 
 def load_steering_model():
-    model_path=os.path.join("models","sterring_angle.h5")
-    model=load_model(model_path,custom_objects={'mae': 'mean_absolute_error'})
+    model = ort.InferenceSession(
+    "models/sterring_angle.onnx",
+    providers=["CUDAExecutionProvider"]   # GPU
+)
+    
     return model
 
 
@@ -48,24 +51,34 @@ def interpret_object_detection_results(result, img,name_map):
         
 def simulate_video_processing():
     # frame 1
-    start_time=time.time()
     cv2.namedWindow("org_video")
     cv2.namedWindow("sterring")
     cv2.namedWindow("object_detection")
     cv2.namedWindow("lane_detection")
     
+    # actual prediction df
+    df=pd.read_csv("./data/raw/07012018/data.csv")
+    
     object_model=load_object_detection_model()
     sterring_model=load_steering_model()
     lane_model=load_lane_detection_model()
     
-    streeing_wheel=cv2.imread(os.path.join("assets","steering_wheel.jpg"))
+    # Get input / output names
+    input_name_sterring = sterring_model.get_inputs()[0].name
+    output_name_sterring = sterring_model.get_outputs()[0].name
+    input_name_lane = lane_model.get_inputs()[0].name
+    output_name_lane = lane_model.get_outputs()[0].name
+    
+    streeing_wheel=cv2.imread(os.path.join("assets","steering_wheel.png"))
     cv2.imshow("sterring",streeing_wheel)
     # steering wheel parameters
     h=200
     w=66
     center = (w // 2, h // 2)
     img_gen=load_image()
+    counter=0
     while True:
+        counter+=1
         try:
             img=next(img_gen)
         except StopIteration:
@@ -80,35 +93,41 @@ def simulate_video_processing():
         img_str=cv2.resize(img_str,(h,w))
         img_str=img_str/255.0 -0.5
         img_str=np.expand_dims(img_str,axis=0)
-        sterring_rad=sterring_model.predict(img_str)[0][0]
-        print("Sterring angle in radians ",sterring_rad)
+        img_str = img_str.astype(np.float32)
+
+        sterring_rad=sterring_model.run([output_name_sterring],{input_name_sterring:img_str})[0][0][0]
         sterring_deg=np.degrees(sterring_rad)
-        print("Sterring angle in degres ",sterring_deg)
         
         M=cv2.getRotationMatrix2D(center, sterring_deg, 1.0)
-        new_sterring_wheel=cv2.warpAffine(streeing_wheel, M, (200, 200))
-        print(new_sterring_wheel.shape)
+        new_sterring_wheel=cv2.warpAffine(streeing_wheel, M, (250, 250))
         cv2.imshow("sterring",new_sterring_wheel)
-
+        print("Actual sterring angle in degrees ",df[df['file_name']==f"{counter-1}.jpg"]['angle']*np.pi/180)
+        print("Time taken to predict steering angle is ",time.time()-start_time)
+        print("Predicted steering angle in degrees ",sterring_deg)
+        
         # object detection
+        start_time=time.time()
         result=object_model.predict(img,conf=0.25,show=False)[0]
         result=sv.Detections.from_ultralytics(result)
         name_map=object_model.model.names
         obj_det_img=interpret_object_detection_results(result,img,name_map)
         cv2.imshow("object_detection",obj_det_img)
+        print("Time taken to predict object detection is ",time.time()-start_time)
+        
         
         # lane detection
+        start_time=time.time()
         img_lan=cv2.resize(img,(224,224))
         img_lan=img_lan/255.0
         img_lan=np.expand_dims(img_lan,axis=0)
-        img_lan=lane_model.predict(img_lan)[0]
+        img_lan=img_lan.astype(np.float32)
+        img_lan=lane_model.run([output_name_lane],{input_name_lane:img_lan})[0][0]
         # img_lan=(img_lan > 0.5).astype(np.uint8)
         cv2.imshow("lane_detection",img_lan)
-        
+        print("Time taken to predict lane detection is ",time.time()-start_time)        
         if cv2.waitKey(100) & 0xFF==ord('q'):
             break
         
-        print("Time taken to predict one frame is ",time.time()-start_time)
         
 if __name__=="__main__":
     simulate_video_processing()
